a45097f18f6c96eb4c4b9564951a2b60111807a6
Leander Jedamus <ljedamus@web.de>
2019-06-08 18:42:46 +0200

    removed LOAD_PATH from lib runden.rb

--- lib/runden.rb
+++ lib/runden.rb
@@ -2,13 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Freitag, 07. Juni 2019 15:19 (C) 2019 von Leander Jedamus
-# modifiziert Samstag, 08. Juni 2019 18:37 von Leander Jedamus
+# modifiziert Samstag, 08. Juni 2019 18:42 von Leander Jedamus
 # modifiziert Freitag, 07. Juni 2019 15:21 von Leander Jedamus
 
 require "English"
-$LOAD_PATH.unshift(ENV["HOME"]+"/Projekte/ruby/lib")
-$LOAD_PATH.unshift(File.expand_path(File.dirname($PROGRAM_NAME)))
-$LOAD_PATH.unshift(".")
 
 def runden(z,s)
   p = 10.0**s
