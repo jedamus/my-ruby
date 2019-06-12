7020092510cfe06c72df6fbc0a82f65dedc60d3d
Leander Jedamus <ljedamus@web.de>
2019-06-12 15:43:38 +0200

    added parseoptions.rb

---
+++ parseoptions.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby -w
+# -*- coding: utf-8 -*-
+
+# erzeugt Mittwoch, 12. Juni 2019 15:14 (C) 2019 von Leander Jedamus
+# modifiziert Mittwoch, 12. Juni 2019 15:38 von Leander Jedamus
+
+require "English"
+require "optparse"
+$LOAD_PATH.unshift(ENV["HOME"]+"/Projekte/ruby/lib")
+$LOAD_PATH.unshift(File.expand_path(File.dirname($PROGRAM_NAME)))
+$LOAD_PATH.unshift(".")
+
+Options = Struct.new(:name)
+
+class Parser
+  def self.parse(options)
+    args = Options.new("world")
+
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: " + File.basename($PROGRAM_NAME) + " [options]"
+
+      opts.on("-nNAME", "--name=NAME", "Name to say hello to") do |n| args.name = n
+      end
+
+      opts.on("-h", "--help", "Prints this help") do
+        puts opts
+        exit
+      end
+    end
+
+    opt_parser.parse!(options)
+    return args
+  end
+end
+# options = Parser.parse %w[--help]
+options = Parser.parse(ARGV)
+puts options
+
+# vim:ai sw=2 sts=4 expandtab
+
