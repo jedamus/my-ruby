1b4c2c69b4d01ce7c86e6bcf661aed2549e71825
Leander Jedamus <ljedamus@web.de>
2019-06-12 19:06:15 +0200

    added gettext

--- check_gettext.rb
+++ check_gettext.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Mittwoch, 12. Juni 2019 16:43 (C) 2019 von Leander Jedamus
+# modifiziert Mittwoch, 12. Juni 2019 19:04 von Leander Jedamus
 # modified Wednesday, 12. June 2019 17:55 by Leander Jedamus
 # modifiziert Mittwoch, 12. Juni 2019 17:46 von Leander Jedamus
 # modified Wednesday, 12. June 2019 17:30 by Leander Jedamus
@@ -13,19 +14,18 @@ $LOAD_PATH.unshift(ENV["HOME"]+"/Projekte/ruby/lib")
 $LOAD_PATH.unshift(File.expand_path(File.dirname($PROGRAM_NAME)))
 $LOAD_PATH.unshift(".")
 
-
-# puts $:
-
-#I18n::Backend::Simple.include(I18n::Backend::Gettext)
+def gettext(s)
+  return(I18n.translate(s))
+end
+alias _ gettext
 
 I18n.load_path += Dir["translate/*.rb"]
 
 lang = ENV["LANG"]
-I18n.locale = :en if lang =~ /C/ 
-I18n.locale = :en if lang =~ /en/ 
+I18n.locale = :en
 I18n.locale = :de if lang =~ /de/ 
 
-puts I18n.translate("gettext")
+puts _("gettext")
 
 # vim:ai sw=2 sts=4 expandtab
 
