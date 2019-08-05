12299c8d0db474820f744cc487d2d4e39a900567
Leander Jedamus <ljedamus@web.de>
2019-08-05 11:30:26 +0200

    added function vergleichen

--- stromrechner_EVM_innogy.rb
+++ stromrechner_EVM_innogy.rb
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Montag, 15. Juli 2019 12:29 (C) 2019 von Leander Jedamus
-# modifiziert Montag, 05. August 2019 10:06 von Leander Jedamus
+# modifiziert Montag, 05. August 2019 11:29 von Leander Jedamus
 # modifiziert Montag, 15. Juli 2019 12:58 von Leander Jedamus
 
 require "English"
@@ -35,6 +35,16 @@ class Energieversorger
 
 end
 
+def vergleichen(anbieter1, anbieter2, obergrenze, delta)
+  print "\n"
+  print anbieter1.putname() + " zu " + anbieter2.putname() + ": "
+  for i in 1.upto(obergrenze)
+    if (anbieter1.berechnen(i)-anbieter2.berechnen(i)).abs < delta then
+      puts i.to_s + " kWh"
+    end
+  end
+end
+
 verbrauch = 10000.0 # kWh
 #verbrauch = 2090.0 # kWh
 verbrauch = 1800.0 # kWh
@@ -42,34 +52,16 @@ evm = Energieversorger.new("EVM", 12 * 11.9, 0.265)
 evm.setverbrauch(verbrauch)
 innogy = Energieversorger.new("innogy", 99.96, 0.2855)
 innogy.setverbrauch(verbrauch)
-innogy_smart = Energieversorger.new("innogy_smart", 99.96, 0.2786)
+innogy_smart = Energieversorger.new("innogy smart", 99.96, 0.2786)
 innogy_smart.setverbrauch(verbrauch)
 
 puts evm
 puts innogy
 puts innogy_smart
-#exit
 
-print "\nevm to innogy: "
-for i in 1.upto(10000)
-  if (evm.berechnen(i)-innogy.berechnen(i)).abs < 0.01 then
-    puts i.to_s + " kWh"
-  end
-end
+vergleichen(evm, innogy, 10000, 0.01)
+vergleichen(evm, innogy_smart, 10000, 0.01)
+vergleichen(innogy, innogy_smart, 10000, 0.01)
 
-print "\nevm to innogy_smart: "
-for i in 1.upto(10000)
-  if (evm.berechnen(i)-innogy_smart.berechnen(i)).abs < 0.01 then
-    puts i.to_s + " kWh"
-  end
-end
-
-print "\ninnogy to innogy_smart: "
-for i in 1.upto(10000)
-  if (innogy.berechnen(i)-innogy_smart.berechnen(i)).abs < 0.01 then
-    puts i.to_s + " kWh"
-  end
-end
-  
 # vim:ai sw=2 sts=4 expandtab
 
