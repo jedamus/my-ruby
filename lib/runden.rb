d2d97a5905c46723e6a4903e8c6fdb13278db392
Leander Jedamus <ljedamus@web.de>
2019-08-05 10:00:49 +0200

    minor changes

--- lib/runden.rb
+++ lib/runden.rb
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Freitag, 07. Juni 2019 15:19 (C) 2019 von Leander Jedamus
-# modifiziert Montag, 05. August 2019 09:42 von Leander Jedamus
+# modifiziert Montag, 05. August 2019 09:57 von Leander Jedamus
 # modifiziert Samstag, 08. Juni 2019 18:42 von Leander Jedamus
 # modifiziert Freitag, 07. Juni 2019 15:21 von Leander Jedamus
 
@@ -10,15 +10,18 @@ require "English"
 
 def runden(z,s)
   p = 10.0**s
-  return((z*p+0.5).truncate / p)
+  z2 = (z*p+0.5).truncate
+  return(z2 / p)
 end
 
 def runden_s(z,s)
   p = 10.0**s
   z2 = z*p
   str = ((z2+0.5).truncate / p).to_s
-  for i in 2.upto(s)
+  z2 = z2.ceil
+  for i in 1.upto(s-1)
     p = 10**i
+    # print "i=", i, ", z2 = ", z2, "\n"
     if (z2 % p) == 0 then
       str = str + "0"
     else
