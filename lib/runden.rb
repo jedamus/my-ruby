7b382a4912a4cd856f4ffbd8faa0ce989bc9bb00
Leander Jedamus <ljedamus@web.de>
2019-08-05 09:40:56 +0200

    added function runden_s

--- lib/runden.rb
+++ lib/runden.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Freitag, 07. Juni 2019 15:19 (C) 2019 von Leander Jedamus
+# modifiziert Montag, 05. August 2019 09:40 von Leander Jedamus
 # modifiziert Samstag, 08. Juni 2019 18:42 von Leander Jedamus
 # modifiziert Freitag, 07. Juni 2019 15:21 von Leander Jedamus
 
@@ -12,7 +13,24 @@ def runden(z,s)
   return((z*p+0.5).truncate / p)
 end
 
+def runden_s(z,s)
+  p = 10.0**s
+  z2 = z*p
+  str = ((z2+0.5).truncate / p).to_s
+  for i in 2.upto(s)
+    print i,z2,"\n"
+    p = 10**i
+    if (z2 % p) == 0 then
+      str = str + "0"
+    else
+      break
+    end
+  end
+  return str
+end
+
 #print(runden(1234.5678,2),"\n")
+#print(runden_s(1234.0,4),"\n")
 
 # vim:ai sw=2 sts=4 expandtab
 
