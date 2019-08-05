c44d80c41de26fb1b96d76a66c3cb8758c855402
Leander Jedamus <ljedamus@web.de>
2019-08-05 09:24:03 +0200

    added to_s-function

--- stromrechner_EVM_innogy.rb
+++ stromrechner_EVM_innogy.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Montag, 15. Juli 2019 12:29 (C) 2019 von Leander Jedamus
+# modifiziert Montag, 05. August 2019 09:21 von Leander Jedamus
 # modifiziert Montag, 15. Juli 2019 12:58 von Leander Jedamus
 
 require "English"
@@ -19,20 +20,35 @@ class Energieversorger
   def putname()
     return(@name)
   end
+
+  def setverbrauch(verbrauch)
+    @verbrauch = verbrauch
+  end
   
   def berechnen(verbrauch)
     return(@grundpreis + verbrauch * @arbeitspreis)
   end
 
+  def to_s()
+    return @name + ": " + runden(berechnen(@verbrauch),2).to_s + " €"
+  end
+
 end
 
 verbrauch = 10000.0 # kWh
-verbrauch = 2090.0 # kWh
+#verbrauch = 2090.0 # kWh
+verbrauch = 1800.0 # kWh
 evm = Energieversorger.new("EVM", 12 * 11.9, 0.265)
+evm.setverbrauch(verbrauch)
 innogy = Energieversorger.new("innogy", 99.96, 0.2855)
+innogy.setverbrauch(verbrauch)
+innogy_smart = Energieversorger.new("innogy", 99.96, 0.2786)
+innogy_smart.setverbrauch(verbrauch)
 
-puts evm.putname + ": " + runden(evm.berechnen(verbrauch),2).to_s
-puts innogy.putname + ": " + runden(innogy.berechnen(verbrauch),2).to_s
+puts evm
+puts innogy
+puts innogy_smart
+#exit
 
 for i in 1.upto(10000)
   if (evm.berechnen(i)-innogy.berechnen(i)).abs < 0.01 then
