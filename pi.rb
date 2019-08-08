837959d48c96e43578b3442f34c3075372d9f7c2
Leander Jedamus <ljedamus@web.de>
2019-08-08 15:13:12 +0200

    added include to pi.rb

--- pi.rb
+++ pi.rb
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Donnerstag, 08. August 2019 09:27 (C) 2019 von Leander Jedamus
-# modifiziert Donnerstag, 08. August 2019 09:28 von Leander Jedamus
+# modifiziert Donnerstag, 08. August 2019 15:12 von Leander Jedamus
 
 require "English"
 $LOAD_PATH.unshift(ENV["HOME"]+"/Projekte/ruby/lib")
 $LOAD_PATH.unshift(File.expand_path(File.dirname($PROGRAM_NAME)))
 $LOAD_PATH.unshift(".")
 
-print 4*Math::atan(1.0),"\n"
-print Math::PI,"\n"
+include Math
+print 4*atan(1.0),"\n"
+print PI,"\n"
 
 # vim:ai sw=2 sts=4 expandtab
 
