5f3e33a717a0e91991572105ee59be5953f9c3ce
Leander Jedamus <ljedamus@web.de>
2021-01-16 16:58:07 +0100

    added translation to check.rb

--- check_gettext.rb
+++ check_gettext.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Mittwoch, 12. Juni 2019 16:43 (C) 2019 von Leander Jedamus
+# modifiziert Samstag, 16. Januar 2021 16:54 von Leander Jedamus
 # modifiziert Donnerstag, 13. Juni 2019 18:41 von Leander Jedamus
 # modifiziert Mittwoch, 12. Juni 2019 21:36 von Leander Jedamus
 # modified Wednesday, 12. June 2019 17:55 by Leander Jedamus
@@ -13,9 +14,10 @@ require "English"
 $LOAD_PATH.unshift(ENV["HOME"]+"/Projekte/ruby/lib")
 $LOAD_PATH.unshift(File.expand_path(File.dirname($PROGRAM_NAME)))
 $LOAD_PATH.unshift(".")
-ENV["LANG"] = "en_US"
+#ENV["LANG"] = "en_US"
 require "my_gettext"
 
+puts _("translate")
 puts _("gettext")
 
 # vim:ai sw=2 sts=4 expandtab
--- locale/check_gettext/check.rb
+++ locale/check_gettext/check.rb
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Donnerstag, 13. Juni 2019 09:09 (C) 2019 von Leander Jedamus
+# modifiziert Samstag, 16. Januar 2021 16:56 von Leander Jedamus
 # modifiziert Donnerstag, 13. Juni 2019 09:09 von Leander Jedamus
 
 {
-  :de => { 'gettext' => 'txetteg' },
-  :en => { 'gettext' => 'GETTEXT' },
+  :en => { 'gettext' => 'GETTEXT',
+           'translate' => 'translate'
+         },
+  :de => { 'gettext' => 'txetteg',
+           'translate' => 'übersetzen'
+         },
 }
 
 # vim:ai sw=2 sts=4 expandtab
