c7b295427272db8c02e40a3316043e7df6ff5186
Leander Jedamus <ljedamus@web.de>
2021-01-23 16:29:40 +0100

    minor changes

--- locale/ruby/check_gettext/check.rb
+++ locale/ruby/check_gettext/check.rb
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # erzeugt Donnerstag, 13. Juni 2019 09:09 (C) 2019 von Leander Jedamus
+# modifiziert Samstag, 23. Januar 2021 16:29 von Leander Jedamus
 # modifiziert Samstag, 16. Januar 2021 17:06 von Leander Jedamus
 # modifiziert Donnerstag, 13. Juni 2019 09:09 von Leander Jedamus
 
 {
-  :en => { 'gettext' => 'GETTEXT',
+  :en => {
+           'gettext' => 'GETTEXT',
            'translate' => 'translate',
          },
-  :de => { 'gettext' => 'txetteg',
+  :de => {
+           'gettext' => 'txetteg',
            'translate' => 'übersetzen',
          },
 }
